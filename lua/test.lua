0419c082aed79a99f06b5c098dc76190bcd782dd
Nickolsky <54820249+rombintu@users.noreply.github.com>
2020-09-28 14:34:59 +0300

    Delete test.lua

--- lua/test.lua
+++
@@ -1,8 +0,0 @@
-function check(yearC, monthC, dayC)
-    local now_date = os.time()
-    local date_burned = os.time{year=yearC, month=monthC, day=dayC}
-    local date_f = now_date - date_burned
-    print(os.date("Вы прожили "..date_f// 60 // 60 // 24 .." дней"))
-end
-    
-check(1999, 4, 20)
